173428a539574131c1f7b154157ef564276a2ccb
VanTran2611 <tvvqna@gmail.com>
2021-08-04 11:17:23 +0700

    first Navbar.css and Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Router } from 'react-router-dom';
 import './App.css';
-
+import './components/Navbar';
+import { Navbar } from './components/Navbar';
 function App() {
   return (
     <div className="App">
